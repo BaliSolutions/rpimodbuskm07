53ef769a2bc999264eb4844b6bba6f2e4fae9415
btnottrue <boom_thirawut@msn.com>
2015-12-20 21:38:08 +0700

    asdf

--- testmodbus.py
+++ testmodbus.py
@@ -11,13 +11,13 @@ pf1=0.0
 while True:
 	try:
 		v1 = instrument.read_long(257,4,False)
+		time.sleep(1)
 		pf1 = instrument.read_register(0,0,4,True)
 	except IOError:
 		print("Failed to read from instrument")
 	except ValueError:
 		print("Checksum error")
 	v1=v1/10.0
-	print  v1
+	print v1
 	pf1=pf1/1000.0
 	print pf1
-	time.sleep(1)
