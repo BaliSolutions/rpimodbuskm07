796f4488654012a51c5afbc652aa4eb431e6cfe0
btnottrue <boom_thirawut@msn.com>
2015-12-21 19:48:31 +0700

    add timestamp

--- testmodbus.py
+++ testmodbus.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import minimalmodbus
 import time
+import datetime
 
 instrument = minimalmodbus.Instrument('/dev/ttyAMA0', 1, mode='rtu') # port name, slave address (in decimal)
 instrument.serial.baudrate=9600
@@ -13,7 +14,6 @@ def readvoltage():
 	while True:
 		try:
 			v1 = (instrument.read_long(257,4,False))/10.0
-			#time.sleep(0.5)
 		except:
 			print ("Got some readvoltage read error")
 			continue
@@ -25,7 +25,6 @@ def readpf():
 	while True:
 		try:
 			pf1 = (instrument.read_register(0,0,4,True))/1000.0
-			#time.sleep(0.5)
 		except:
 			print ("Got some Power Factor read error")
 			continue
@@ -35,17 +34,10 @@ def readpf():
 	return pf1
 
 while True:
+	print datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S') #timestamp starttime
 	volt=readvoltage()
 	pf=readpf()
-	"""try:
-		#v1 = (instrument.read_long(257,4,False))/10.0
-		#time.sleep(0.5)
-		#pf1 = (instrument.read_register(0,0,4,True))/1000.0
-		#time.sleep(0.5)
-	except IOError:
-		print("Failed to read from instrument")
-	except ValueError:
-		print("Checksum error")"""
 	print volt
 	print pf
+	print datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S') #timestamp finishtime
 	time.sleep(10)
