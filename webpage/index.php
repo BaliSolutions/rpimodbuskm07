fe2a31e1101555c2e67937f0cb518635a794866f
btnottrue <boom_thirawut@msn.com>
2016-03-17 05:32:44 +0700

    asdf

--- webpage/index.php
+++ webpage/index.php
@@ -14,7 +14,7 @@
 	while($row = mysql_fetch_array($retval,MYSQL_ASSOC))
 	{
 		echo 	"Date : {$row['date']} Time : {$row['time']} <br>".
-				"{$row['volt']} Volt {$row['amp']} Amp PF={$row['pf']} Readed KVA={$row['kva']} <br>"
+				"{$row['volt']} Volt {$row['amp']} Amp<br>"
 				"------------------------------------------<br>";
 	}
 	mysql_close($conn);
