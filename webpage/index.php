01ff129283929360c25d276df7b9e0646959825c
btnottrue <boom_thirawut@msn.com>
2016-03-17 05:26:19 +0700

    asdf

--- webpage/index.php
+++ webpage/index.php
@@ -8,14 +8,14 @@
 	$password = "password";
 	$db = "km07";
 	$conn = mysql_connect($host,$user,$password);
-	$sql = "SELECT date, time FROM rawdata WHERE date='2016-03-17' AND time BETWEEN '02:00:00' and '03:40:00'";
+	$sql = "SELECT * FROM rawdata WHERE date='2016-03-17' AND time BETWEEN '02:00:00' and '03:40:00'";
 	mysql_select_db(km07);
 	$retval = mysql_query($sql,$conn);
 	while($row = mysql_fetch_array($retval,MYSQL_ASSOC))
 	{
-		echo 	"REPLIED <br>".
-				"Date : {$row['date']} <br>".
-				"Time : {$row['time']} <br>";
+		echo 	"Date : {$row['date']} Time : {$row['time']} <br>".
+				"{$row['volt']} Volt {$row['amp']} Amp PF={$row['pf']} Readed KVA={$row['kva']} <br>"
+				"------------------------------------------";
 	}
 	mysql_close($conn);
 ?>
