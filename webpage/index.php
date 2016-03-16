df1c4e1f971bf3dfb7842958f5a375e72292c47b
btnottrue <boom_thirawut@msn.com>
2016-03-17 05:02:21 +0700

    Revert "asdf"
    This reverts commit a60406e33133e1c6c6378498b05b32d2d63bc86a.

--- webpage/index.php
+++ webpage/index.php
@@ -8,8 +8,14 @@
 	$password = "password";
 	$db = "km07";
 	$conn = mysql_connect($host,$user,$password);
+<<<<<<< HEAD
 	$sql = "SELECT * FROM rawdata";
 
+||||||| a60406e... asdf
+	$sql = "SELECT date, time FROM rawdata WHERE date=\'2016-03-17\' AND time BETWEEN \'02:00:00\' and \'03:40:00\'";
+=======
+	$sql = "SELECT * FROM rawdata WHERE date='2016-03-17' AND time BETWEEN '02:00:00' and '03:40:00'";
+>>>>>>> parent of a60406e... asdf
 	mysql_select_db(km07);
 	$retval = mysql_query($sql,$conn);
 	while($row = mysql_fetch_array($retval,MYSQL_ASSOC))
