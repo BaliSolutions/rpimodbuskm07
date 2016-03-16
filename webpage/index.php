f6cfb9d8091ce8161a4e3e61fb9ce2d93ce96d3d
btnottrue <boom_thirawut@msn.com>
2016-03-17 04:28:18 +0700

    asdf

--- webpage/index.php
+++ webpage/index.php
@@ -10,6 +10,13 @@
 	$conn = mysql_connect($host,$user,$password);
 	$sql = 'SELECT date, time, volt, amp, pf, kva FROM rawdata';
 	mysql_select_db(km07);
+	$retval = mysql_query($sql,$conn);
+	while($row = mysql_fetch_array($retval,MYSQL_ASSOC))
+	{
+		echo 	"REPLIED <br>".
+				"Date : {$row['date']} <br>".
+				"Time : {$row['time']} <br>"
+	}
 ?>
 
 
