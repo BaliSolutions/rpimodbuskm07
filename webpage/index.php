8a9f7e50a255d3a77e1a0dea70282b6b1030895c
btnottrue <boom_thirawut@msn.com>
2016-03-17 04:55:33 +0700

    asdf

--- webpage/index.php
+++ webpage/index.php
@@ -8,14 +8,16 @@
 	$password = "password";
 	$db = "km07";
 	$conn = mysql_connect($host,$user,$password);
+
 	$sql = "SELECT * FROM rawdata";
+
 	mysql_select_db(km07);
 	$retval = mysql_query($sql,$conn);
 	while($row = mysql_fetch_array($retval,MYSQL_ASSOC))
 	{
-		echo 	"Date : {$row['date']} ".
-				"Time : {$row['time']} <br>"
-
+		echo 	"REPLIED <br>".
+				"Date : {$row['date']} <br>".
+				"Time : {$row['time']} <br>";
 	}
 	mysql_close($conn);
 ?>
