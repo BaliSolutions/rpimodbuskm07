4375c0184bad91e3554761482de9cc61c8f83cc3
btnottrue <boom_thirawut@msn.com>
2016-03-17 05:49:00 +0700

    change filename

--- webpage/index.php
+++ webpage/index.php
@@ -2,23 +2,61 @@
 <html>
 <body>
 
-<?php
-	$host = "localhost";
-	$user = "root";
-	$password = "password";
-	$db = "km07";
-	$conn = mysql_connect($host,$user,$password);
-	$sql = "SELECT date,time,volt,amp,pf,kva FROM rawdata WHERE date='2016-03-17' AND time BETWEEN '02:00:00' and '03:40:00'";
-	mysql_select_db(km07);
-	$retval = mysql_query($sql,$conn);
-	while($row = mysql_fetch_array($retval,MYSQL_ASSOC))
-	{
-		echo 	"Date : {$row['date']} Time : {$row['time']} <br>".
-				"{$row['volt']} Volt {$row['amp']} Amp PF={$row['pf']} Readed KVA={$row['kva']} <br>".
-				"------------------------------------------<br>";
-	}
-	mysql_close($conn);
-?>
+<form action="result.php">
+ <select name="day">
+	<option value="1">1</option>
+	<option value="2">2</option>
+	<option value="3">3</option>
+	<option value="4">4</option>
+	<option value="5">5</option>
+	<option value="6">6</option>
+	<option value="7">7</option>
+	<option value="8">8</option>
+	<option value="9">9</option>
+	<option value="10">10</option>
+	<option value="11">11</option>
+	<option value="12">12</option>
+	<option value="13">13</option>
+	<option value="14">14</option>
+	<option value="15">15</option>
+	<option value="16">16</option>
+	<option value="17">17</option>
+	<option value="18">18</option>
+	<option value="19">19</option>
+	<option value="20">20</option>
+	<option value="21">21</option>
+	<option value="22">22</option>
+	<option value="23">23</option>
+	<option value="24">24</option>
+	<option value="25">25</option>
+	<option value="26">26</option>
+	<option value="27">27</option>
+	<option value="28">28</option>
+	<option value="29">29</option>
+	<option value="30">30</option>
+	<option value="31">31</option>
+</select>
+<select name="month">
+	<option value="01">January</option>
+	<option value="02">Febuary</option>
+	<option value="03">March</option>
+	<option value="04">April</option>
+	<option value="05">May</option>
+	<option value="06">June</option>
+	<option value="07">July</option>
+	<option value="08">August</option>
+	<option value="09">September</option>
+	<option value="10">October</option>
+	<option value="11">November</option>
+	<option value="12">December</option>
+</select>
+
+ <select name="year">
+	<option value="2016">2011</option>
+</select>
+  <br><br>
+  <input type="submit">
+</form>
 
 
 </body>
---
+++ webpage/result.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+	$host = "localhost";
+	$user = "root";
+	$password = "password";
+	$db = "km07";
+	$conn = mysql_connect($host,$user,$password);
+	$sql = "SELECT date,time,volt,amp,pf,kva FROM rawdata WHERE date='2016-03-17' AND time BETWEEN '02:00:00' and '03:40:00'";
+	mysql_select_db(km07);
+	$retval = mysql_query($sql,$conn);
+	while($row = mysql_fetch_array($retval,MYSQL_ASSOC))
+	{
+		echo 	"Date : {$row['date']} Time : {$row['time']} <br>".
+				"{$row['volt']} Volt {$row['amp']} Amp PF={$row['pf']} Readed KVA={$row['kva']} <br>".
+				"------------------------------------------<br>";
+	}
+	mysql_close($conn);
+?>
+
+
+</body>
+</html>
