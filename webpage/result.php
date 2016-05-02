a5e00708cd8a3a75d47f4032676a867459484998
btnottrue <boom_thirawut@msn.com>
2016-05-03 00:53:17 +0700

    asdf

--- webpage/result.php
+++ webpage/result.php
@@ -30,15 +30,15 @@ From <input type="time" name="start_time" value="<?php echo $_POST["start_time"]
 	$sql = "SELECT date,time,volt,amp,pf,kva FROM rawdata WHERE date='$date' AND time BETWEEN '$start_time' and '$stop_time'";
 	mysql_select_db(km07);
 	$retval = mysql_query($sql,$conn);
-	/*while($row = mysql_fetch_array($retval,MYSQL_ASSOC))
+	while($row = mysql_fetch_array($retval,MYSQL_ASSOC))
 	{
 		echo 	"Date : {$row['date']} Time : {$row['time']} <br>".
-				"{$row['volt']} Volt {$row['amp']} Amp PF={$row['pf']} Readed KVA={$row['kva']} <br>".
+				"{$row['volt']} Volt {$row['amp']} Amp PF={$row['pf']} Readed KVA={$row['kva']} kWh={$row['kwh']} <br>".
 				"------------------------------------------<br>";
-	}*/
+	}
 	$time = array();
 	$kwh = array();
-	while($row = mysql_fetch_array($retval,MYSQL_ASSOC))
+	/*while($row = mysql_fetch_array($retval,MYSQL_ASSOC))
 	{
 		$n=0;
 		$time[$n] = $row['time'];
@@ -46,7 +46,7 @@ From <input type="time" name="start_time" value="<?php echo $_POST["start_time"]
 		echo 	"{$row['time']} {$row['kwh']} <br>".
 				"{$time[$n]} {$kwh[$n]} <br>";
 		$n++;
-	}
+	}*/
 	mysql_close($conn);
 ?>
 
