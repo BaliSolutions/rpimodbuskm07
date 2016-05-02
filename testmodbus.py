a278c6812234d97042f141ffd2eb354430ff9cf4
btnottrue <boom_thirawut@msn.com>
2016-05-02 22:10:56 +0700

    asdf

--- testmodbus.py
+++ testmodbus.py
@@ -99,14 +99,17 @@ def readkVA():
 	return kva1
 def readTotalkWh():
 	while True:
+		print "Try to read"
 		try:
 			time.sleep(0.004)
 			if (instrument.read_register(537,0,4,False))==1:
 				mul=1.0
 			else :
 				mul=1000.0
+			print "Read 537 OK %s" %(mul)
 			time.sleep(0.004)
 			kexp = instrument.read_register(536,0,4,False)
+			print "Read 536 OK %s" %(kexp)
 			time.sleep(0.004)
 			totalkwh = (instrument.read_long(543,4,True))*mul/math.pow(10,-1.0*kexp)
 		except:
