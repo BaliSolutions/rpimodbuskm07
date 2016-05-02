9148b4777e5e9d4373e6d204ed40599072c23e60
btnottrue <boom_thirawut@msn.com>
2016-05-03 01:21:23 +0700

    asdf

--- webpage/result.php
+++ webpage/result.php
@@ -38,9 +38,9 @@ From <input type="time" name="start_time" value="<?php echo $_POST["start_time"]
 	}*/
 	$time = array();
 	$kwh = array();
+	$n=0;
 	while($row = mysql_fetch_array($retval,MYSQL_ASSOC))
 	{
-		$n=0;
 		$time[$n] = $row['time'];
 		$kwh[$n] = $row['kwh'];
 		$n++;
