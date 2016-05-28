e6d2f969f77c17a9a9661b2432d6c08d7240f8b3
btnottrue <boom_thirawut@msn.com>
2016-05-29 04:26:10 +0700

    new page

---
+++ webpage/realtime.php
@@ -0,0 +1,11 @@
+<?php
+  $host = "localhost";
+  $user = "root";
+  $password = "password";
+  $db = "km07";
+  $conn = mysql_connect($host,$user,$password);
+  $sql = "SELECT * FROM realtime WHERE id=1";
+  mysql_select_db(km07);
+  $retval = mysql_query($sql,$conn);
+  echo $retval;
+?>
